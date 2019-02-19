3260a46e2e3e6b73b5dcb775fffbf89eeb895b10
Martyn Janes <martyn@obany.com>
2019-02-19 11:29:47 +0300

    Switch app to use REACT_APP_CONFIG_ID

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -38,7 +38,7 @@ class App extends Component<RouteComponentProps, AppState> {
     public async componentDidMount(): Promise<void> {
         try {
             const configService = new ConfigurationService<IConfiguration>();
-            const configId = process.env.CONFIG_ID || "local";
+            const configId = process.env.REACT_APP_CONFIG_ID || "local";
             const config = await configService.load(`/data/config.${configId}.json`);
 
             ServiceFactory.register("configuration", () => configService);
