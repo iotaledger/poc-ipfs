e5cc4e6097f4d1a460b2cfea815db337877b1dc8
Martyn Janes <martyn@obany.com>
2019-04-03 14:49:49 +0300

    Remove config logging

--- api/src/utils/appHelper.ts
+++ api/src/utils/appHelper.ts
@@ -51,7 +51,6 @@ export class AppHelper {
 
                 console.log(`Started API Server on port ${port} v${packageJson.version}`);
                 console.log(`Running Config '${configId}'`);
-                console.log(config);
 
                 if (onComplete) {
                     onComplete(app, config, port);
