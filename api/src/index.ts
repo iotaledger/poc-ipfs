526cffd5dbce68b6a3fb004e1b520baf2c781693
Martyn Janes <martyn@obany.com>
2019-04-11 18:46:41 +0300

    Remove config

--- api/src/index.ts
+++ api/src/index.ts
@@ -4,8 +4,7 @@ import { AppHelper } from "./utils/appHelper";
 const routes: IRoute[] = [
         { path: "/init", method: "get", func: "init" },
         { path: "/ipfs", method: "post", func: "ipfsStore" },
-        { path: "/ipfs", method: "get", func: "ipfsRetrieve" },
-        { path: "/config", method: "get", inline: async (cfg) => cfg }
+        { path: "/ipfs", method: "get", func: "ipfsRetrieve" }
     ];
 
 AppHelper.build(routes);
