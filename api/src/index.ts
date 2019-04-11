d7e6d10faa60623db9aab34e3eb81d81a9d6f03c
Martyn Janes <martyn@obany.com>
2019-04-11 18:26:46 +0300

    Updated config

--- api/src/index.ts
+++ api/src/index.ts
@@ -4,7 +4,8 @@ import { AppHelper } from "./utils/appHelper";
 const routes: IRoute[] = [
         { path: "/init", method: "get", func: "init" },
         { path: "/ipfs", method: "post", func: "ipfsStore" },
-        { path: "/ipfs", method: "get", func: "ipfsRetrieve" }
+        { path: "/ipfs", method: "get", func: "ipfsRetrieve" },
+        { path: "/config", method: "get", inline: async (cfg) => cfg }
     ];
 
 AppHelper.build(routes);
