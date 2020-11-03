2a1f1455d5fd0e2d13ff2ba969e8079650992dd7
Martyn Janes <martyn@obany.com>
2020-11-03 12:24:31 +0100

    Update deployment configuration

--- client/src/services/tangleExplorerService.ts
+++ client/src/services/tangleExplorerService.ts
@@ -18,16 +18,6 @@ export class TangleExplorerService {
     }
 
     /**
-     * Open a bundle hash in the explorer.
-     * @param bundleHash The bundle hash.
-     */
-    public bundle(bundleHash?: string): void {
-        if (bundleHash) {
-            window.open(this._config.bundles.replace(":bundleHash", bundleHash), "_blank");
-        }
-    }
-
-    /**
      * Open a transaction hash in the explorer.
      * @param transactionHash The transaction hash.
      */
